308c8d05de1469eabbbd96ae71695b9e119988b7
Joao Damazio <joaodamazio98@hotmail.com>
2019-10-02 08:44:59 -0300

    Adicionado layout base

--- app/src/main/java/com/deck/yugioh/Activities/LoginActivity.java
+++ app/src/main/java/com/deck/yugioh/Activities/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.deck.yugioh;
+package com.deck.yugioh.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.deck.yugioh.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
