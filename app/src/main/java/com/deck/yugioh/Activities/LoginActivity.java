5992bba3a72e38ef4ce381bd027ee7d3a3137421
Joao Damazio <joaodamazio98@hotmail.com>
2019-10-18 09:54:39 -0300

    Adicionado componente para loading

--- app/src/main/java/com/deck/yugioh/Activities/LoginActivity.java
+++ app/src/main/java/com/deck/yugioh/Activities/LoginActivity.java
@@ -37,8 +37,6 @@ public class LoginActivity extends AppCompatActivity {
 
         this.submitBtn = findViewById(R.id.loginBtn);
 
-
-
     }
 
     @Override
--- app/src/main/java/com/deck/yugioh/Components/LoadingView.java
+++ app/src/main/java/com/deck/yugioh/Components/LoadingView.java
@@ -8,26 +8,43 @@ import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.deck.yugioh.R;
 
-public class LoadingView extends View {
+public class LoadingView extends ConstraintLayout {
 
     public LoadingView(Context context) {
         super(context);
+        this.init(context);
     }
 
     public LoadingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.init(context);
     }
 
     public LoadingView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        this.init(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed,l,t,r,b);
     }
 
 
-    private void init(){
+    private void init(Context context){
+
+        inflate(context, R.layout.sample_loading_view, this);
+
+
 
 //        new Timer().scheduleAtFixedRate(new TimerTask(){
 //            @Override
