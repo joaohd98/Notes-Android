78fe94133786011bc5df55f291bf83c6375583d1
Joao Damazio <joaodamazio98@hotmail.com>
2019-10-18 10:11:50 -0300

    Adicionado loading

--- app/src/main/java/com/deck/yugioh/Components/LoadingView.java
+++ app/src/main/java/com/deck/yugioh/Components/LoadingView.java
@@ -1,23 +1,20 @@
 package com.deck.yugioh.Components;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.deck.yugioh.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class LoadingView extends ConstraintLayout {
+
+    private TextView textView;
 
     public LoadingView(Context context) {
         super(context);
@@ -34,35 +31,29 @@ public class LoadingView extends ConstraintLayout {
         this.init(context);
     }
 
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        super.onLayout(changed,l,t,r,b);
-    }
-
-
     private void init(Context context){
 
-        inflate(context, R.layout.sample_loading_view, this);
+        View view = inflate(context, R.layout.sample_loading_view, this);
 
+        this.textView = view.findViewById(R.id.loading_text);
 
+        new Timer().scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
 
-//        new Timer().scheduleAtFixedRate(new TimerTask(){
-//            @Override
-//            public void run(){
-//
-//                String txt = textView.getText().toString();
-//
-//                if(txt.length() > 15)
-//                    txt = defaultText;
-//
-//                else
-//                    txt += ".";
-//
-//                textView.setText(txt);
-//
-//            }
-//
-//        }, 0, 1000);
+                String txt = textView.getText().toString();
+
+                if(txt.length() > 12)
+                    txt = getResources().getString(R.string.components_loading_text);
+
+                else
+                    txt += ".";
+
+                textView.setText(txt);
+
+            }
+
+        }, 0, 1000);
 
     }
 }
