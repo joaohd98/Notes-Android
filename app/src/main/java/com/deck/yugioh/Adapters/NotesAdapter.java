79a895b85774bfe50d011aeec001698ecd986855
Joao Damazio <joaodamazio98@hotmail.com>
2019-10-24 17:27:08 -0300

    Adicionado ultimate recycle view

--- app/src/main/java/com/deck/yugioh/Adapters/NotesAdapter.java
+++ app/src/main/java/com/deck/yugioh/Adapters/NotesAdapter.java
@@ -11,10 +11,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.deck.yugioh.Model.Notes.NotesView;
 import com.deck.yugioh.Model.Notes.NotesViewHolder;
 import com.deck.yugioh.R;
+import com.marshalchen.ultimaterecyclerview.UltimateViewAdapter;
 
 import java.util.ArrayList;
 
-public class NotesAdapter extends RecyclerView.Adapter<NotesViewHolder> {
+public class NotesAdapter extends UltimateViewAdapter<NotesViewHolder> {
 
     private Context context;
     private ArrayList<NotesView> notes;
@@ -24,15 +25,15 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesViewHolder> {
         this.context = context;
     }
 
-    @NonNull
     @Override
-    public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public NotesViewHolder onCreateViewHolder(ViewGroup parent) {
 
         View view = LayoutInflater.from(this.context).inflate(R.layout.adapter_notes, parent, false);
 
         return new NotesViewHolder(view);
 
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
@@ -46,8 +47,35 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesViewHolder> {
     }
 
     @Override
-    public int getItemCount() {
+    public NotesViewHolder newFooterHolder(View view) {
+        return null;
+    }
+
+    @Override
+    public NotesViewHolder newHeaderHolder(View view) {
+        return null;
+    }
+
+    @Override
+    public int getAdapterItemCount() {
         return notes.size();
     }
+
+    @Override
+    public long generateHeaderId(int position) {
+        return 0;
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void onBindHeaderViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
 
 }
